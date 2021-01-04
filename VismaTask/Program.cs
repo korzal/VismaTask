0d89c4a04a020674669f52c240c41439aaea0b05
korzal <ceernelius@inbox.lt>
2021-01-04 13:41:23 +0200

    initial commit

---
+++ VismaTask/Program.cs
@@ -0,0 +1,16 @@
+﻿using CsvHelper;
+using System;
+using System.IO;
+using System.Text;
+
+namespace VismaTask
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
